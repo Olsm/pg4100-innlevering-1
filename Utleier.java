42ec2d8ef08441391e5870a15a2533e59584a799
OlavOlsm <OlavOlsm@gmail.com>
2016-01-21 17:17:50 +0100

    Create arraylist for available and rented cars

--- Utleier.java
+++ Utleier.java
@@ -1,9 +1,14 @@
 package innlevering1;
 
+import java.util.ArrayList;
+
 public class Utleier {
+	ArrayList<Leiebil> carsAvailable;
+	ArrayList<Leiebil> carsRented;
 	
 	public Utleier () {
-		
+		carsAvailable = new ArrayList<Leiebil> (10);
+		carsRented = new ArrayList<Leiebil> (10);
 	}
 	
 	public void leie () {
