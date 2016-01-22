85a0a3a30e8ff2fbad5282ac6bcd741c30b816a8
OlavOlsm <OlavOlsm@gmail.com>
2016-01-22 13:23:57 +0100

    create initial application code

--- LeiebilApp.java
+++ LeiebilApp.java
@@ -1,12 +1,21 @@
 package innlevering1;
 
+import java.util.Scanner;
+
 public class LeiebilApp {
+	private static Utleier utleier = new Utleier();
 	
 	public LeiebilApp () {
-		
+		utleier = new Utleier();
 	}
 	
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Kunde 1: ");
+			Kunde kunde = new Kunde(utleier, in.nextLine());
+			new Thread(kunde).start();
+		}
+		in.close();
 	}
 }
