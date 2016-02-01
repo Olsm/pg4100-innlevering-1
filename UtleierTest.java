5427f0539b858bcb526861f3363fb8f8249ca275
OlavOlsm <OlavOlsm@gmail.com>
2016-02-01 15:46:43 +0100

    fix junit test

--- UtleierTest.java
+++ UtleierTest.java
@@ -13,39 +13,38 @@ public class UtleierTest {
 	CarRentalAgency rentalAgency;
 	CarRentalAgency mockRentalAgency;
 	RentalCar car;
+	Customer customer;
 	
 	@Before
 	public void setup () {
 		rentalAgency = new CarRentalAgency(); 
 		mockRentalAgency = mock (CarRentalAgency.class);
 		car = rentalAgency.getCarsAvailable().get(0);
+		customer = new Customer(rentalAgency, null, "Olav");
 	}
 
 	@Test
 	public void testRentAndDeliver () {
 		// Test rent
-		assertEquals(car, rentalAgency.rent());
+		assertEquals(car, rentalAgency.rent(customer));
 		assertFalse(rentalAgency.getCarsAvailable().contains(car));
-		assertTrue(rentalAgency.getCarsRented().contains(car));
 		
 		// Test deliver
-		rentalAgency.deliver(car);
+		rentalAgency.deliver(customer, car);
 		assertTrue(rentalAgency.getCarsAvailable().contains(car));
-		assertFalse(rentalAgency.getCarsRented().contains(car));
 	}
 	
 	@Test
 	public void testRentNotAvailable () {
 		when(mockRentalAgency.getCarsAvailable()).thenReturn(new ArrayList<RentalCar>());
-		assertEquals(null, mockRentalAgency.rent());
+		assertEquals(null, mockRentalAgency.rent(customer));
 	}
 	
 	@Test
 	public void testDeliverCarNotInRented () {
 		car = new RentalCar("NotInUtleier");
-		rentalAgency.deliver(car);
+		rentalAgency.deliver(customer, car);
 		assertFalse(rentalAgency.getCarsAvailable().contains(car));
-		assertFalse(rentalAgency.getCarsRented().contains(car));
 	}
 
 }
